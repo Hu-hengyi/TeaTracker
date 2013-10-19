cf7c3e038af4e12a4a89ca4cf86a6e331b0ad286
Daniel Roth <dwroth@gmail.com>
2013-10-19 15:51:30 -0700

    Removing secretToken from gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeaTracker::Application.config.secret_token = '0165e65e1b8ecca0338721deac8ad505c56440234899c16e5401fb426bc4ad9a1d544455ffcd379a7d14fb15287da77d6fe51408ae3e963f57776a463034d95c'
