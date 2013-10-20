4844c4f7663f34d2dfea24f2b75afbad40d989c9
saasbook <latheren007@gmail.com>
2013-10-20 00:51:13 -0700

    Features in progress: add_edit_farm

--- features/add_edit_farm.feature
+++ features/add_edit_farm.feature
@@ -5,10 +5,10 @@ Feature: add a farm with its inputted data
   I want to be able the gathered data of a farm online
 
 Scenario: add a new farm and its info
-  Given I am on the farm list page
-  When I click "Add new farm"
+  Given I am on the FarmList home page
+  When I follow "Add new farm"
   Then I should be on the Create New Farm page
   When I enter in basic farm data
-  And I click "Create farm"
-  Then I should see basic farm
-  
+  And I press "Create farm"
+  Then I will see the basic farm
+
---
+++ features/step_definitions/list_web_steps.rb
@@ -0,0 +1,50 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
+
+# Visiting and Clicking
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
+When /^(?:|I )go to (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
+When /^(?:|I )follow "([^"]*)"$/ do |link|
+  click_link(link)
+end
+
+When /^(?:|I )press "([^"]*)"$/ do |button|
+  click_button(button)
+end
+
+Then /^I should be on (.+)$/ do |page_name|
+  current_path.should == path_to(page_name)
+end
+
+
+
+#Field Entry
+
+When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  fill_in(field, :with => value)
+end
+
+# Use this to fill in an entire form with data from a table. Example:
+#
+#   When I fill in the following:
+#     | Account Number | 5002       |
+#     | Expiry date    | 2009-11-01 |
+#     | Note           | Nice guy   |
+#     | Wants Email?   |            |
+#
+# TODO: Add support for checkbox, select or option
+# based on naming conventions.
+#
+When /^(?:|I )fill in the following:$/ do |fields|
+  fields.rows_hash.each do |name, value|
+    When %{I fill in "#{name}" with "#{value}"}
+  end
+end
