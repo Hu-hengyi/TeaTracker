15b55e00f3c49becb4a8f425f070f05673f471cd
Samir Makhani <makhani@berkeley.edu>
2013-10-20 23:19:59 -0700

    fixed merge issues in application.rb

--- config/application.rb
+++ config/application.rb
@@ -58,10 +58,5 @@ module TeaTracker
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-<<<<<<< HEAD
-=======
-
-    config.assets.initialize_on_precompile = false
->>>>>>> ff16f598bc1171f36aa9905d9c5aae278474ec46
   end
 end
