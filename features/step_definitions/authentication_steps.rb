5b4388f5ff0f17d0b2e91bbd48304ea0b929eb1d
Daniel Roth <dwroth@gmail.com>
2013-11-01 15:40:50 -0700

    Adds user authentication feature specs and steps.  Also adds some functionalities to list_web_steps.rb

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I am not logged in$/) do
+  pending
+end
+
+Given(/^there exists a user "([^"]*)" with password "([^"]*)"$/) do
+  pending
+end
+
+Given (/^(?:that )?there exists a farm named "([^"]*)"$/) do |arg|
+  pending
+end
+
+Given(/^I am logged in as "([^"]*)"$/) do |arg|
+  pending
+end
