7f3f46843eaa8b6f4d79826cc351891f66c723f4
Daniel Roth <dwroth@gmail.com>
2013-11-25 09:09:02 -0800

    precompiling assets

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,6 @@ module TeaTracker
 
     config.assets.precompile += ['reset.css']
     config.assets.precompile += ['devise/sessions.css']
-    config.assets.precompile += []
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
