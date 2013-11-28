f343ad7896c202719c95949def5427d1e26661ab
saasbook <ruchika.gupta@berkeley.edu>
2013-11-27 17:48:19 -0800

    added list of possible qualities and drop down select for quality

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,4 +1,8 @@
 class Deposit < ActiveRecord::Base
   attr_accessible :quality, :weight, :farm_id
   belongs_to :farm
+  def self.all_qualities
+    ["Very Good","Good","OK", "Bad", "Very Bad" ]
+  end
+
 end
