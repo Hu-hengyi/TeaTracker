e658f8574cc1d74a8d2e414cff9a7674df8290f1
saasbook <ruchika.gupta@berkeley.edu>
2013-12-03 21:34:59 -0800

    fixed schema for collection point

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(:version => 20131203165958) do
 
   create_table "cps", :force => true do |t|
     t.string   "name"
+    t.float    "longitude"
+    t.float    "latitude"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
