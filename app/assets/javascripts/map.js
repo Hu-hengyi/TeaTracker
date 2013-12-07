8170d9c616abb3b2ccbc8adc2bff8c88319a9a77
saasbook <ruchika.gupta@berkeley.edu>
2013-12-07 10:51:15 -0800

    added hooks to leaf images in js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,5 +1,24 @@
+
 $(window).load(function() {
   var map = L.map('map').setView(new L.LatLng(-16.026181, 35.645142), 13);
+
+  var LeafIcon = L.Icon.extend({
+      options: {
+          shadowUrl: 'leaf-shadow.png',
+          iconSize:     [38, 38],
+          shadowSize:   [0, 0],
+          iconAnchor:   [22, 94],
+          shadowAnchor: [4, 62],
+          popupAnchor:  [-3, -76]
+      }
+  });
+
+  var oneLeafIcon = new LeafIcon({iconUrl: '../../assets/two-leaf.png'});
+  var twoLeafIcon = new LeafIcon({iconUrl: '../../assets/one-leaves.png'});
+  var threeLeafIcon = new LeafIcon({iconUrl: '../../assets/three-leaves.png'});
+  L.icon = function (options) {
+      return new L.Icon(options);
+  };
 
   /* Set up map */
   L.tileLayer('http://b.tile.cloudmade.com/8ee2a50541944fb9bcedded5165f09d9/114586/256/{z}/{x}/{y}.png', {
@@ -12,12 +31,15 @@ $(window).load(function() {
     var lat = parseFloat($(this).html());
     $('.longitude a').each(function(){
       var lon = parseFloat($(this).html());
-      L.marker(new L.LatLng(lat, lon)).addTo(map)
+      L.marker(new L.LatLng(lat, lon), {icon:oneLeafIcon}).addTo(map)
       .bindPopup(index.toString())      
     });
   });
 
-  /* Set leaflet-marker-icon source */
+
+
+  /* Set leaflet-marker-icon source 
   $('.leaflet-marker-icon').attr('src', 'http://leafletjs.com/dist/images/marker-icon.png');
-  $('.leaflet-marker-shadow').attr('src', 'http://leafletjs.com/dist/images/marker-shadow.png');
+  $('.leaflet-marker-shadow').attr('src', '');
+  */
 });
