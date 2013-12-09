7c8b8418637eae620753de4b0c9e73f65306db41
Daniel Roth <dwroth@gmail.com>
2013-12-09 10:37:37 -0800

    Changes map to satellite view.  Looks amazing

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,6 +1,11 @@
 
 $(window).load(function() {
-  var map = L.map('map').setView(new L.LatLng(-16.026181, 35.645142), 13);
+
+  var map = L.map('map', {
+      layers: MQ.satelliteLayer(),
+      center: [-15.97857, 35.65389],
+      zoom: 12
+  });//.setView(new L.LatLng(-16.026181, 35.645142), 13);
 
   var LeafIcon = L.Icon.extend({
       options: {
@@ -21,10 +26,10 @@ $(window).load(function() {
   };
 
   /* Set up map */
-  L.tileLayer('http://b.tile.cloudmade.com/8ee2a50541944fb9bcedded5165f09d9/114586/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
-  }).addTo(map);
+//  L.tileLayer('http://b.tile.cloudmade.com/8ee2a50541944fb9bcedded5165f09d9/114586/256/{z}/{x}/{y}.png', {
+//    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+//    maxZoom: 18
+//  }).addTo(map);
 
   /* Drop markers for farms */
   $('.latitude a').each(function(index){
