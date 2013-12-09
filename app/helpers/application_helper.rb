e77e09f1aa7d7c918f7b21d7e1e71c0ebc7d98c9
Daniel Roth <dwroth@gmail.com>
2013-12-09 12:48:02 -0800

    Moves hybrid map logic to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,11 @@ module ApplicationHelper
     end
   end
 
+  def hybridMap
+    content_for(:head) do
+      javascript_include_tag "http://beta.mapquestapi.com/sdk/leaflet/v0.1/mq-map.js?key=Fmjtd%7Cluubn161nl%2C2x%3Do5-90awg6"
+    end
+  end
+
 
 end
